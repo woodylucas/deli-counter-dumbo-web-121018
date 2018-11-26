fc99eabdcabfc20087f94f3686ee346f0d2fb200
Woody Lucas <wlucas0311@gmail.com>
2018-11-26 07:37:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,4 +19,11 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end 
 
-
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  end
+end
