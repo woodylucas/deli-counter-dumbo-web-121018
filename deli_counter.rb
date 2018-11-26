be26122b0ab78bbc950719c3a163f2ba060200bb
Woody Lucas <wlucas0311@gmail.com>
2018-11-26 07:33:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 def line(katz_deli)
   postion_in_line = []
   if katz_deli.empty?
-  puts "The line is currently empty"
+  puts "The line is currently empty."
 else 
   number = 1 
   katz_deli.each do |customer|
